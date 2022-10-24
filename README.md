c898677965d1a47ed41e1b49ff76a35e77ffcbf8
Abhinav Sinha <imonline.abhinav@gmail.com>
2022-10-25 01:07:05 +0530

    Help Text for CLI

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # **Cryptile**
-### A **Desktop App** and **CLI tool** to encrypt and decrypt your files with a secure password :lock:
+### A **Desktop App** and **CLI tool** to encrypt and decrypt your files with a secure password :lock:. Military grade fast encryption with almost zero overhead.
 
 ## (in develpoment)
 
--- cargo-cryptile/src/config.rs
+++ cargo-cryptile/src/config.rs
@@ -25,6 +25,9 @@ pub struct Config<'a> {
 
 impl<'a> Config<'a> {
     pub fn parse(args: &Vec<String>) -> Result<Config, &str> {
+        
+        // Config Parsing Logic
+
         return Err(HELP_TEXT);
     }
 }
@@ -32,23 +35,23 @@ impl<'a> Config<'a> {
 
 
 const HELP_TEXT: &str = "\
-                    A Command line tool for encrypting and decrypting your files.\n\
-                    Still In Development. Won't Work\n\
-                    \n\
-                    Usage:\n\
-                    \tcryptile [COMMAND] [FLAGS]\n\
-                    Commands:\n\
-                    \tencrypt <FILENAME> [PASSWORD_OPTIONS]       Encrypt file using given password\n\
-                    \tdecrypt <FILENAME> [PASSWORD_OPTIONS]       Decrypt file using given password\n\
-                    \t    Password Options:\n\
-                    \t        -p <PASSWORD>                       Specify a password\n\
-                    \t        -s, --saved <SAVED_IDENTIFIER>      Use a saved password using it's identifier\n\
-                    \t        -m, --master                        Use the master password (if set)\n\
-                    \tset [SET_OPTIONS]\n\
-                    \t    Set Options:\n\
-                    \t        -m, --master                        Set a master password\n\
-                    \t        -p                                  Save a password and it's identifier name\n\
-                    Flags:\n\
-                    \t-h, --help                                  Display this help information\n\
-                    \t--replace                                   Remove the original file after Encryption/Decryption\n\
-                      ";
+        A Command line tool for encrypting and decrypting your files.\n\
+        (Still In Development. Won't Work.)\n\
+        \n\
+        Usage:\n\
+        \tcryptile [COMMAND] [FLAGS]\n\
+        Commands:\n\
+        \tencrypt <FILENAME> [PASSWORD_OPTIONS]       Encrypt file using given password\n\
+        \tdecrypt <FILENAME> [PASSWORD_OPTIONS]       Decrypt file using given password\n\
+        \t    Password Options:\n\
+        \t        -p <PASSWORD>                       Specify a password\n\
+        \t        -s, --saved <SAVED_IDENTIFIER>      Use a saved password using it's identifier\n\
+        \t        -m, --master                        Use the master password (if set)\n\
+        \tset [SET_OPTIONS]\n\
+        \t    Set Options:\n\
+        \t        -m, --master                        Set a master password\n\
+        \t        -p                                  Save a password and it's identifier name\n\
+        Flags:\n\
+        \t-h, --help                                  Display this help information\n\
+        \t--replace                                   Remove the original file after Encryption/Decryption\n\
+          ";
